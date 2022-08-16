f017dbcc9215a141d1cc5ec4c84d8a866f60993f
usbbush <jeanbienvenusb@gmail.com>
2022-08-16 16:32:07 +0200

    Update redux store configuration

--- src/redux/combine-reducers.js
+++ src/redux/combine-reducers.js
@@ -2,9 +2,9 @@ import { combineReducers } from '@reduxjs/toolkit';
 import bookReducer from './books/books';
 import categoryReducer from './categories/categories';
 
-const reducers=combineReducers({
-  book:bookReducer,
-  category:categoryReducer,
+const reducers = combineReducers({
+  books: bookReducer,
+  categories: categoryReducer,
 });
 
-export default  reducers
+export default reducers;
--- src/redux/configure-store.js
+++ src/redux/configure-store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reducers from './combine-reducers';
-const store = configureStore({reducer:reducers});
 
-export default  store
+const store = configureStore({ reducer: reducers });
+
+export default store;
