659ab4a791ee08df4b2d551c84c68f656cff3c24
usbbush <jeanbienvenusb@gmail.com>
2022-08-19 17:06:51 +0200

    Add navbar profile-icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import '../styles/navbar.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { BsPersonCircle } from 'react-icons/bs';
 
 const Navbar = () => (
   <nav className="nav" style={{ display: 'flex' }}>
@@ -9,6 +10,9 @@ const Navbar = () => (
     </h2>
     <Link to="/" className="App-link">Books</Link>
     <Link to="/categories" className="App-link opacity-5">Categories</Link>
+    <div style={{ flexGrow: 1, textAlign: 'end' }}>
+      <BsPersonCircle style={{ color: '#0290ff', fontSize: '2rem' }} />
+    </div>
   </nav>
 );
 export default Navbar;
