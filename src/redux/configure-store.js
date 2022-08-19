02fe42e57f82b31dc95857eb456049122186d7f7
usbbush <jeanbienvenusb@gmail.com>
2022-08-19 10:46:15 +0200

    Create middleware

--- src/redux/configure-store.js
+++ src/redux/configure-store.js
@@ -1,5 +1,6 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
 import reducers from './combine-reducers';
 
-const store = configureStore({ reducer: reducers });
+const store = configureStore({ reducer: reducers }, applyMiddleware(thunk));
 export default store;
