4613768c16351ef8b89a2d012f75a7b2aec1e35b
usbbush <jeanbienvenusb@gmail.com>
2022-08-19 10:52:33 +0200

    Load books from  api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import Navbar from './components/Navbar';
 import Categories from './pages/categories';
 import Books from './pages/books';
+import { loadBook } from './redux/books/books';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadBook());
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
