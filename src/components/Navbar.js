f48fd9912c2888b257ab92c8351b1aba4eebe704
usbbush <jeanbienvenusb@gmail.com>
2022-08-19 11:51:23 +0200

    Style navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,14 @@
+import '../styles/navbar.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => (
   <nav className="nav">
+    <span className="Bookstore-CMS Text-Style-7">
+      Bookstore CMS
+    </span>
     <Link to="/" className="App-link">Books</Link>
-    <Link to="/categories" className="App-link">Categories</Link>
+    <Link to="/categories" className="App-link opacity-5">Categories</Link>
   </nav>
 );
 export default Navbar;
