7657f31ebe4748590e02b49d58ac378878bd99a0
usbbush <jeanbienvenusb@gmail.com>
2022-08-24 23:54:05 +0200

    Refactor before deploy

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/book-store">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
